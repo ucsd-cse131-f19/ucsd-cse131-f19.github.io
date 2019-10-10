327f646113c560d06d11edd5cca6839da60960c9
Joe Gibbs Politz <joe.politz@gmail.com>
2019-10-10 12:09:23 -0700

    from class

--- lectures/10-10-lec5/compile.ml
+++ lectures/10-10-lec5/compile.ml
@@ -59,33 +59,26 @@ let rec find (env : tenv) (x : string) : int option =
     | (y, i)::rest ->
       if y = x then Some(i) else find rest x
 
-
+let true_const = "0xFFFFFFFFFFFFFFFE"
+let false_const = "0x7FFFFFFFFFFFFFFE"
 let rec e_to_is (e : expr) (si : int) (env : tenv) =
   match e with
-    | ENum(i) -> [sprintf "mov rax, %d" (i * 2 + 1)]
-    | EBool(true) -> ["mov rax, 0xFFFFFFFFFFFFFFFE"]
-    | EBool(false) -> ["mov rax, 0x7FFFFFFFFFFFFFFE"]
+    | ENum(i) -> [sprintf "mov rax, %d" ((i * 2) + 1)]
+    | EBool(true) -> [sprintf "mov rax, %s" true_const]
+    | EBool(false) -> [sprintf "mov rax, %s" false_const]
     | EPlus(e1, e2) ->
-      let e1is = e_to_is e1 si env in
-      let e2is = e_to_is e2 (si + 1) env in
-      e1is @
-      [sprintf "mov %s, rax" (stackval si)] @
-      e2is @
-       (* Check that the right-hand operand is a number *)
-      [sprintf "mov %s, rax" (stackval (si + 1));
-       "and rax, 1";
-       "cmp rax, 1";
-       "jne error"; (* Jump to code that can call a C function and exit *)
-       sprintf "mov rax, %s" (stackval si);
-       sprintf "add rax, %s" (stackval (si + 1));
-       sprintf "sub rax, 1"
-       ]
-
-
-
-
-
-
+      (* Note that this only checks the type on the e1 *)
+      let e1_is = e_to_is e1 si env in
+      let e2_is = e_to_is e2 (si + 1) env in
+      let store_e1 = (sprintf "mov [rsp-%d], rax" (stackloc si)) in
+      let check_e1 = [
+        "and rax, 1"; "cmp rax, 0"; "je op_error"
+      ] in
+      (* Note that we check _after_ we store to make sure we store before doing the `and` *)
+      e1_is @ [store_e1] @ check_e1 @ e2_is @[
+        sprintf "and rax, %s" true_const;
+        sprintf "add rax, [rsp-%d]" (stackloc si)
+      ]
 
 
     | EId(x) ->
@@ -106,23 +99,24 @@ let rec e_to_is (e : expr) (si : int) (env : tenv) =
         | Dec -> arg_exprs @ ["sub rax, 2"]
 
 
-(* Compiles a source program string to an x86 string *)
 let compile (program : string) : string =
   let ast = parse program in
   let instrs = e_to_is ast 1 [] in 
   let instrs_str = (String.concat "\n" instrs) in
+  (* The `push 0` below is to make sure that `rsp` ends up 8-byte aligned.
+     It is a total hack that we will improve when we talk about calling
+     conventions in more detail. *)
   sprintf "
 section .text
 global our_code_starts_here
-extern print_error_and_exit
+extern print_err_exit
 our_code_starts_here:
   %s
   ret
-  error:
-    mov rdi, 500
-    push 0
-    call print_error_and_exit
-    ; CALL A C FUNCTION CALLED print_error_and_exit
+
+op_error:
+  push 0
+  call print_err_exit
   \n" instrs_str
 
 let () =
--- lectures/10-10-lec5/main.c
+++ lectures/10-10-lec5/main.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int64_t our_code_starts_here()
-  asm("our_code_starts_here");
-extern void print_error_and_exit(int64_t errcode) asm("print_error_and_exit");
+extern int64_t our_code_starts_here() asm("our_code_starts_here");
+extern void print_err_exit() asm("print_err_exit");
 
-void print_error_and_exit(int64_t errcode) {
-  printf("Error: %lld\n", errcode);
+// Extensions for more specific error messages
+void print_op_error() {}
+void print_if_error() {}
+
+void print_err_exit(int64_t what_value) {
+  printf("Error!\n");
   exit(1);
 }
 
 int main(int argc, char** argv) {
   int64_t result = our_code_starts_here();
-  if((result & 1) == 1) {
-    printf("%lld\n", result >> 1);
-  }
-  else if(result == 0x7FFFFFFFFFFFFFFE) {
-    printf("false\n");
+  if((result & 1)) {
+    printf("%lld\n", (result - 1) / 2);
   }
   else if(result == 0xFFFFFFFFFFFFFFFE) {
     printf("true\n");
   }
-  else {
-    printf("Unknown value: %lld\n", result);
+  else if(result == 0x7FFFFFFFFFFFFFFE) {
+    printf("false\n");
   }
-  printf("\nThe rep was: %lld\n", result);
+  else {
+    printf("Got unrerpresentable value: %lld", result);
+  }
   return 0;
 }
